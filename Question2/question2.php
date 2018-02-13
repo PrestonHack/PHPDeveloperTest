0aac28f9fe691906e94e0e043e74255a45ad7bc4
Preston1337 <preston.hackett@okstate.edu>
2018-02-12 18:40:18 -0600

    I think this is correct as per the examples given
    I actually wrote a lot of code defeated myself then tried the
    array_unique function and it appears to give the appropriate result
    however I am counting the sequence which I am not sure is the right
    approach. Can't determine if it wants the sequence counted or the number
    of times  a individual fish was caught.

--- Question2/question2.php
+++ Question2/question2.php
@@ -2,10 +2,9 @@
 $json = file_get_contents('https://liquid.fish/fishes.json');
 $fishData = json_decode($json, true);
 $fish = array();
-
 foreach ($fishData as $val)
 {
-    $key = implode(",", $val['fish_caught']);    
+    $key = implode(",", array_unique($val['fish_caught']));    
     if(!array_key_exists($key, $fish))
     {
         $fish[$key] = 1;
@@ -19,15 +18,12 @@ arsort($fish);
 $topResults = (array_slice($fish,0 ,20));
 echo "The top 20 sequences of fish caught <br/>";
 $i = 1;
-
 foreach($topResults as $key => $value)
 {   
     echo "#$i" . " $key was caught $value times. <br/>";
     $i++;
 }
-
 echo "All catches and counts <br/>";
-
 foreach($fish as $key => $value)
 {   
     echo "$key was caught $value times. <br/>";    
